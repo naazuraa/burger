3ca4a4725368f6d3b3b0f3ce9735a7fa76717b25
Nazura Ramli <nazura97@gmail.com>
2022-04-23 14:36:56 +0800

    day 13

--- pages/index.vue
+++ pages/index.vue
@@ -85,17 +85,20 @@
 					/>
 				</div>
 			</div>
-			<div class="grid grid-cols-12 font-oswald uppercase text-4xl text-yellow-800">
-				<div class="col-span-5">
+			<div class="grid grid-cols-12 font-oswald uppercase text-4xl text-yellow-800 px-40 gap-5">
+				<div class="col-span-5 bg-cover bg-center bg-[url('/fries-bg-1.jpg')] pl-20 pb-10 pt-10">
 					<h4>Get your free</h4>
 					<h4>Cheese Fries</h4>
 					<button class="uppercase bg-red-500 text-white text-lg py-3 px-8 mt-5">Claim now</button>
 				</div>
-				<div class="col-span-7">
-					<h4>Crispy Chicken</h4>
-					<h4>Burger is back!</h4>
+				<div class="col-span-7 bg-cover bg-center bg-[url('/burger-bg-1.jpg')] text-right pr-10 pt-10">
+					<h4>Crispy <br />Chicken</h4>
+					<h4>Burger <br />is back!</h4>
 					<button class="uppercase bg-red-500 text-white text-lg py-3 px-8 mt-5">Claim now</button>
 				</div>
+			</div>
+			<div class="grid grid-cols-3 px-40 mt-5 gap-5">
+				<img src="/offer-1.jpg" alt=""><img src="/offer-2.jpg" alt=""><img src="/offer-3.jpg" alt="">
 			</div>
 		</div>
 	</div>
